6d004cda001788b11dcf3a91061c0db4b1ee45d6
dandreev <Azazello1992Kot!>
2025-07-15 23:02:17 +0300

    добавил хлебные крошки

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,20 @@ import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from './shared/components/header/header.component';
 import {HomeComponent} from './features/home/home.component';
 import {FooterComponent} from './shared/components/footer/footer.component';
+import {BreadcrumbsComponent} from './shared/components/breadcrumbs/breadcrumbs.component';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, HomeComponent, FooterComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    imports: [
+        RouterOutlet,
+        HeaderComponent,
+        HomeComponent,
+        FooterComponent,
+        BreadcrumbsComponent,
+    ],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'arman';
+    title = 'arman';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+        data: { breadcrumb: 'Главная' },
+    },
+    {
+        path: 'organizers',
+        loadComponent: () =>
+            import('./features/organizers/organizers.component').then(
+                (m) => m.OrganizersComponent,
+            ),
+        data: { breadcrumb: 'О лиге - Наша команда' },
+    },
+];
